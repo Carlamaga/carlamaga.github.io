2bd28fb8eb4a620d04e3c6058a69a323767aec7e
unknown <carla.a.magallanes.11.cm@gmail.com>
2023-03-01 13:59:26 -0300

    Carga de CV

---
+++ index.js
@@ -0,0 +1,23 @@
+function verMas(){
+    console.log("estoy dentro de la funcion")
+    Swal.fire(
+        'Perfil de LinkedIn',
+        'Carla Agustina Magallanes ',
+        
+      )
+}
+function contacto1(){
+Swal.fire(
+'2604859575',
+)
+}
+function contacto2(){
+    Swal.fire(
+    '2604498027',
+    )
+}   
+function contacto3(){
+    Swal.fire(
+    '2604809070',
+    )
+ }
